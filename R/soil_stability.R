5e963866f813a7259efeb2054e47ae1a43fb67ad
Knight <aknight@usgs.gov>
2019-09-12 16:39:24 -0600

    added by_year functionality to soil_stability()

--- R/soil_stability.R
+++ R/soil_stability.R
@@ -5,6 +5,8 @@
 #' @param uncovered. Logical. When \code{TRUE}, calculates soil stability for samples not covered by perennial vegetation. Defaults to \code{TRUE}
 #' @param all_cover_types Logical. When \code{TRUE}, calculates soil stability for each indidual cover type. Defaults to \code{FALSE}
 #' @param tall Logical. Indicates if output is tall/long or wide. Defaults to \code{TRUE}
+#' @param by_year Logical. If \code{TRUE} then results will be reported further
+#' grouped by year using the \code{FormDate} field from the data forms. Defaults to \code{FALSE}
 #' @return Dataframe of calculated soil stability values by plot.
 
 #' @export soil_stability
@@ -14,8 +16,13 @@ soil_stability <- function(soil_stability_tall,
                            cover = TRUE,
                            uncovered = TRUE,
                            all_cover_types = FALSE,
-                           tall = FALSE) {
+                           tall = FALSE,
+                           by_year = FALSE) {
   soil_stability_rating <- list()
+  # add Year column if grouping by year
+  if(by_year){
+    soil_stability_tall$Year <- lubridate::year(soil_stability_tall$FormDate)
+  }
 
   # Remove NA Rating values
   soil_stability_tall <- subset(soil_stability_tall, !is.na(Rating))
@@ -23,7 +30,12 @@ soil_stability <- function(soil_stability_tall,
   # Calculate a mean rating for all cover types
   if (all == TRUE) {
     soil_stability_rating[["all"]] <- soil_stability_tall %>%
-      dplyr::group_by(PrimaryKey) %>%
+      {if(by_year){
+        dplyr::group_by(., PrimaryKey, Year)
+      }else{
+        dplyr::group_by(., PrimaryKey)
+        }
+      }%>%
       dplyr::summarize(rating = mean(Rating)) %>%
       dplyr::mutate(Veg = "all") %>%
       as.data.frame()
@@ -32,7 +44,12 @@ soil_stability <- function(soil_stability_tall,
   if (cover == TRUE) {
     soil_stability_rating[["covered"]] <- soil_stability_tall %>%
       subset(Veg != "NC") %>%
-      dplyr::group_by(PrimaryKey) %>%
+      {if(by_year){
+        dplyr::group_by(., PrimaryKey, Year)
+      }else{
+        dplyr::group_by(., PrimaryKey)
+        }
+      }%>%
       dplyr::summarize(rating = mean(Rating)) %>%
       dplyr::mutate(Veg = "covered") %>%
       as.data.frame()
@@ -41,7 +58,12 @@ soil_stability <- function(soil_stability_tall,
   if (uncovered == TRUE) {
     soil_stability_rating[["unconvered"]] <- soil_stability_tall %>%
       subset(Veg == "NC") %>%
-      dplyr::group_by(PrimaryKey) %>%
+      {if(by_year){
+        dplyr::group_by(., PrimaryKey, Year)
+      }else{
+        dplyr::group_by(., PrimaryKey)
+        }
+      }%>%
       dplyr::summarize(rating = mean(Rating)) %>%
       dplyr::mutate(Veg = "uncovered") %>%
       as.data.frame()
@@ -49,7 +71,12 @@ soil_stability <- function(soil_stability_tall,
   # Calculate mean rating for all cover types individually
   if (all_cover_types == TRUE) {
     soil_stability_rating[["all_cover_types"]] <- soil_stability_tall %>%
-      dplyr::group_by(PrimaryKey, Veg) %>%
+      {if(by_year){
+        dplyr::group_by(., PrimaryKey, Year, Veg)
+      }else{
+        dplyr::group_by(., PrimaryKey, Veg)
+        }
+      }%>%
       dplyr::summarize(rating = mean(Rating)) %>%
       as.data.frame()
   }
